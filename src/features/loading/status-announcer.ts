1aa829d9fc9aa6abfea03722bcc60725abc1559d
Chris Joel <cdata@google.com>
2019-02-05 09:16:31 -0800

    Sparsely announce loading status to screen readers

---
+++ src/features/loading/status-announcer.ts
@@ -0,0 +1,180 @@
+/*
+ * Copyright 2019 Google Inc. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the 'License');
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an 'AS IS' BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import ModelViewerElementBase from '../../model-viewer-base.js';
+import {debounce} from '../../utils.js';
+
+export const INITIAL_STATUS_ANNOUNCEMENT =
+    'This page includes one or more 3D models that are loading';
+export const FINISHED_LOADING_ANNOUNCEMENT =
+    'All 3D models in the page have loaded';
+export const UPDATE_STATUS_DEBOUNCE_MS = 100;
+
+const $modelViewerStatusInstance = Symbol('modelViewerStatusInstance');
+const $updateStatus = Symbol('updateStatus');
+
+interface InstanceLoadingStatus {
+  instanceWasUnregistered: () => void;
+  loadAttemptCompletes: Promise<any>;
+}
+
+/**
+ * The LoadingStatusAnnouncer manages announcements of loading status across
+ * all <model-viewer> elements in the document at any given time. As new
+ * <model-viewer> elements are connected to the document, they are registered
+ * with a LoadingStatusAnnouncer singleton. As they are disconnected, the are
+ * also unregistered. Announcements are made to indicate the following
+ * conditions:
+ *
+ *  1. There are <model-viewer> elements that have yet to finish loading
+ *  2. All <model-viewer> elements in the page have finished attempting to load
+ */
+export class LoadingStatusAnnouncer {
+  /**
+   * The "status" instance is the <model-viewer> instance currently designated
+   * to announce the loading status of all <model-viewer> elements in the
+   * document at any given time. It might change as <model-viewer> elements are
+   * attached or detached over time.
+   */
+  protected[$modelViewerStatusInstance]: ModelViewerElementBase|null = null;
+
+  protected registeredInstanceStatuses:
+      Map<ModelViewerElementBase, InstanceLoadingStatus> =
+          new Map<ModelViewerElementBase, InstanceLoadingStatus>();
+
+  protected loadingPromises: Array<Promise<any>> = [];
+
+  /**
+   * This element is a node that floats around the document as the status
+   * instance changes (see above). It is a singleton that represents the loading
+   * status for all <model-viewer> elements currently in the page. It has its
+   * role attribute set to "status", which causes screen readers to announce
+   * any changes to its text content.
+   *
+   * @see https://www.w3.org/TR/wai-aria-1.1/#status
+   */
+  readonly statusElement: HTMLParagraphElement = document.createElement('p');
+  protected statusUpdateInProgress: boolean = false;
+
+  protected[$updateStatus]: () => void =
+      debounce(() => this.updateStatus(), UPDATE_STATUS_DEBOUNCE_MS);
+
+  constructor() {
+    this.statusElement.setAttribute('role', 'status');
+    this.statusElement.style.position = 'absolute';
+    this.statusElement.style.color = 'transparent';
+    this.statusElement.style.pointerEvents = 'none';
+  }
+
+  /**
+   * Register a <model-viewer> element with the announcer. If it is not yet
+   * loaded, its loading status will be tracked by the announcer.
+   */
+  registerInstance(modelViewer: ModelViewerElementBase) {
+    if (this.registeredInstanceStatuses.has(modelViewer)) {
+      return;
+    }
+
+    let instanceWasUnregistered = () => {};
+    const loadShouldBeMeasured =
+        modelViewer.loaded === false && !!(modelViewer as any).src;
+    const loadAttemptCompletes = loadShouldBeMeasured ?
+        new Promise((resolve) => {
+          const resolveHandler = () => {
+            resolve();
+
+            modelViewer.removeEventListener('load', resolveHandler);
+            modelViewer.removeEventListener('error', resolveHandler);
+          };
+
+          modelViewer.addEventListener('load', resolveHandler);
+          modelViewer.addEventListener('error', resolveHandler);
+
+          instanceWasUnregistered = resolveHandler;
+        }) :
+        Promise.resolve();
+
+    this.registeredInstanceStatuses.set(
+        modelViewer, {instanceWasUnregistered, loadAttemptCompletes});
+
+    this.loadingPromises.push(loadAttemptCompletes);
+
+    if (this.modelViewerStatusInstance == null) {
+      this.modelViewerStatusInstance = modelViewer;
+    }
+  }
+
+  /**
+   * Unregister a <model-viewer> element with the announcer. Its loading status
+   * will no longer be tracked by the announcer.
+   */
+  unregisterInstance(modelViewer: ModelViewerElementBase) {
+    if (!this.registeredInstanceStatuses.has(modelViewer)) {
+      return;
+    }
+
+    const statuses = this.registeredInstanceStatuses;
+    const instanceStatus = statuses.get(modelViewer)!;
+    statuses.delete(modelViewer);
+    instanceStatus.instanceWasUnregistered();
+
+    if (this.modelViewerStatusInstance === modelViewer) {
+      this.modelViewerStatusInstance =
+          statuses.size > 0 ? statuses.keys().next().value : null;
+    }
+  }
+
+  protected get modelViewerStatusInstance(): ModelViewerElementBase|null {
+    return this[$modelViewerStatusInstance];
+  }
+
+  protected set modelViewerStatusInstance(value: ModelViewerElementBase|null) {
+    const currentInstance = this[$modelViewerStatusInstance];
+    if (currentInstance === value) {
+      return;
+    }
+
+    const {statusElement} = this;
+
+    if (value != null && value.shadowRoot != null) {
+      value.shadowRoot.appendChild(statusElement);
+    } else if (statusElement.parentNode != null) {
+      statusElement.parentNode.removeChild(statusElement);
+    }
+
+    this[$modelViewerStatusInstance] = value;
+    this[$updateStatus]();
+  }
+
+  protected async updateStatus() {
+    if (this.statusUpdateInProgress || this.loadingPromises.length === 0) {
+      return;
+    }
+
+    console.log('Initial status');
+    this.statusElement.textContent = INITIAL_STATUS_ANNOUNCEMENT;
+    this.statusUpdateInProgress = true;
+
+    while (this.loadingPromises.length) {
+      const {loadingPromises} = this;
+      this.loadingPromises = [];
+      await Promise.all(loadingPromises);
+    }
+
+    console.log('Finished status');
+    this.statusElement.textContent = FINISHED_LOADING_ANNOUNCEMENT;
+    this.statusUpdateInProgress = false;
+  }
+}
---
+++ src/test/features/loading/status-announcer-spec.ts
@@ -0,0 +1,170 @@
+/*
+ * Copyright 2019 Google Inc. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the 'License');
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an 'AS IS' BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import {LoadingMixin} from '../../../features/loading.js';
+import {FINISHED_LOADING_ANNOUNCEMENT, INITIAL_STATUS_ANNOUNCEMENT, LoadingStatusAnnouncer, UPDATE_STATUS_DEBOUNCE_MS} from '../../../features/loading/status-announcer.js';
+import ModelViewerElementBase from '../../../model-viewer-base.js';
+import {assetPath, isInDocumentTree, timePasses, until, waitForEvent} from '../../helpers.js';
+
+const expect = chai.expect;
+
+suite('LoadingStatusAnnouncer', () => {
+  let nextId = 0;
+  let tagName: string;
+  let ModelViewerElement: any;
+  let loadingStatusAnnouncer: LoadingStatusAnnouncer;
+
+  setup(() => {
+    tagName = `model-viewer-loading-announcer-${nextId++}`;
+    ModelViewerElement = class extends LoadingMixin
+    (ModelViewerElementBase) {
+      static get is() {
+        return tagName;
+      }
+    };
+    customElements.define(tagName, ModelViewerElement);
+    loadingStatusAnnouncer = new LoadingStatusAnnouncer();
+  });
+
+  suite('when a model is registered', () => {
+    suite('that has a src', () => {
+      test('sets initial status', async () => {
+        const element = new ModelViewerElement();
+        element.poster = assetPath('Astronaut.png');
+        element.src = assetPath('Astronaut.glb');
+
+        loadingStatusAnnouncer.registerInstance(element);
+
+        await timePasses(UPDATE_STATUS_DEBOUNCE_MS);
+
+        const {statusElement} = loadingStatusAnnouncer;
+
+        expect(statusElement.textContent)
+            .to.be.equal(INITIAL_STATUS_ANNOUNCEMENT);
+      });
+
+      suite('after the model loads', () => {
+        test('sets the finished status', async () => {
+          const element = new ModelViewerElement();
+          element.src = assetPath('Astronaut.glb');
+
+          loadingStatusAnnouncer.registerInstance(element);
+
+          await until(() => element.loaded);
+          await timePasses(UPDATE_STATUS_DEBOUNCE_MS);
+
+          const {statusElement} = loadingStatusAnnouncer;
+
+          expect(statusElement.textContent)
+              .to.be.equal(FINISHED_LOADING_ANNOUNCEMENT);
+        });
+      });
+
+      suite('there are other registered models', () => {
+        test('sets finished status when all models are loaded', async () => {
+          const elementOne = new ModelViewerElement();
+          const elementTwo = new ModelViewerElement();
+
+          elementOne.src = elementTwo.src = assetPath('Astronaut.glb');
+
+          loadingStatusAnnouncer.registerInstance(elementOne);
+          loadingStatusAnnouncer.registerInstance(elementTwo);
+
+          await Promise.all(
+              [until(() => elementOne.loaded), until(() => elementTwo.loaded)]);
+          await timePasses(UPDATE_STATUS_DEBOUNCE_MS);
+
+          const {statusElement} = loadingStatusAnnouncer;
+
+          expect(statusElement.textContent)
+              .to.be.equal(FINISHED_LOADING_ANNOUNCEMENT);
+        });
+
+        suite('one model is already loaded', () => {
+          test('eventually sets finished status', async () => {
+            const elementOne = new ModelViewerElement();
+            const elementTwo = new ModelViewerElement();
+
+            elementOne.src = assetPath('Astronaut.glb');
+
+            await until(() => elementOne.loaded);
+
+            elementTwo.src = assetPath('Astronaut.glb');
+
+            loadingStatusAnnouncer.registerInstance(elementOne);
+            loadingStatusAnnouncer.registerInstance(elementTwo);
+
+            await until(() => elementTwo.loaded);
+            await timePasses(UPDATE_STATUS_DEBOUNCE_MS);
+
+            const {statusElement} = loadingStatusAnnouncer;
+
+            expect(statusElement.textContent)
+                .to.be.equal(FINISHED_LOADING_ANNOUNCEMENT);
+          });
+        });
+
+        suite('one model fails', () => {
+          test('eventually sets finished status', async () => {
+            const elementOne = new ModelViewerElement();
+            const elementTwo = new ModelViewerElement();
+
+            const errorOccurs = waitForEvent(elementOne, 'error');
+
+            elementOne.src = assetPath('DOES_NOT_EXIST.glb');
+            elementTwo.src = assetPath('Astronaut.glb');
+
+            loadingStatusAnnouncer.registerInstance(elementOne);
+            loadingStatusAnnouncer.registerInstance(elementTwo);
+
+            await Promise.all([errorOccurs, until(() => elementTwo.loaded)]);
+            await timePasses(UPDATE_STATUS_DEBOUNCE_MS);
+
+            const {statusElement} = loadingStatusAnnouncer;
+
+            expect(statusElement.textContent)
+                .to.be.equal(FINISHED_LOADING_ANNOUNCEMENT);
+          });
+        });
+
+        suite('first element is removed', () => {
+          test('status element remains in the document tree', async () => {
+            // NOTE(cdata): We use ModelViewerElementBase here because we are
+            // testing behavior that is affected by connected/disconnected
+            // side-effects in LoadingMixin.
+            const elementOne = new ModelViewerElementBase();
+            const elementTwo = new ModelViewerElementBase();
+
+            document.body.appendChild(elementOne);
+            document.body.appendChild(elementTwo);
+
+            const {statusElement} = loadingStatusAnnouncer;
+
+            loadingStatusAnnouncer.registerInstance(elementOne);
+
+            expect(isInDocumentTree(statusElement)).to.be.equal(true);
+
+            loadingStatusAnnouncer.registerInstance(elementTwo);
+
+            loadingStatusAnnouncer.unregisterInstance(elementOne);
+            document.body.removeChild(elementOne);
+
+            expect(isInDocumentTree(statusElement)).to.be.equal(true);
+            document.body.removeChild(elementTwo);
+          });
+        });
+      });
+    });
+  });
+});
