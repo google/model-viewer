89f1293d2a22a76e8fe9c349a2b85c17fda745ec
Chris Joel <cdata@google.com>
2019-04-11 15:03:08 -0700

    Convert ModelViewerElementBase to TypeScript

--- src/types/resizeobserver.d.ts
+++ src/types/resizeobserver.d.ts
@@ -1,4 +1,17 @@
-interface ResizeObserver {
+interface ResizeObserverSize {
+  inlineSize: number;
+  blockSize: number;
+}
+
+interface ResizeObserverEntry {
+  target: Element;
+  contentRect: DOMRectReadOnly;
+  borderBoxSize: ResizeObserverSize;
+  contentBoxSize: ResizeObserverSize;
+}
+
+declare class ResizeObserver {
+  constructor(callback: (entries: Array<ResizeObserverEntry>) => void);
   observe(element: Element): void;
   unobserve(element: Element): void;
   disconnect(): void;
