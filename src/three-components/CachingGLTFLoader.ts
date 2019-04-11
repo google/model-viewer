337a4375ea5f68f6eed2aec8615ece1e99928461
Chris Joel <cdata@google.com>
2019-04-11 16:06:09 -0700

    Convert CachingGLTFLoader to TypeScript

--- src/three-components/CachingGLTFLoader.ts
+++ src/three-components/CachingGLTFLoader.ts
@@ -14,15 +14,25 @@
  */
 
 import GLTFLoader from '../third_party/three/GLTFLoader.js';
-import {cloneGltf} from './ModelUtils.js';
 
-export const loadWithLoader = (url, loader) =>
-    new Promise((resolve, reject) => {
-      loader.load(url, resolve, () => {}, reject);
-    });
+import {cloneGltf, Gltf} from './ModelUtils.js';
 
-const cache = new Map();
-const preloaded = new Map();
+export type ProgressCallback = (loaded: number, total: number) => void;
+
+export const loadWithLoader =
+    (url: string,
+     loader: any,
+     progressCallback: ProgressCallback|null = null) => {
+      const onProgress = progressCallback != null ? (event: ProgressEvent) => {
+        progressCallback!(event.loaded, event.total);
+      } : () => {};
+      return new Promise<Gltf>((resolve, reject) => {
+        loader.load(url, resolve, onProgress, reject);
+      });
+    }
+
+const cache = new Map<string, Promise<Gltf>>();
+const preloaded = new Map<string, boolean>();
 
 export class CachingGLTFLoader {
   static clearCache() {
@@ -30,7 +40,7 @@ export class CachingGLTFLoader {
     preloaded.clear();
   }
 
-  static has(url) {
+  static has(url: string) {
     return cache.has(url);
   }
 
@@ -38,21 +48,19 @@ export class CachingGLTFLoader {
    * Returns true if the model that corresponds to the specified url is
    * available in our local cache.
    */
-  static hasFinishedLoading(url) {
+  static hasFinishedLoading(url: string) {
     return !!preloaded.get(url);
   }
 
-  constructor() {
-    this.loader = new GLTFLoader();
-  }
+  protected loader: GLTFLoader = new GLTFLoader();
 
   /**
    * Preloads a glTF, populating the cache. Returns a promise that resolves
    * when the cache is populated.
    */
-  async preload(url) {
+  async preload(url: string, progressCallback: ProgressCallback|null = null) {
     if (!cache.has(url)) {
-      cache.set(url, loadWithLoader(url, this.loader));
+      cache.set(url, loadWithLoader(url, this.loader, progressCallback));
     }
 
     await cache.get(url);
@@ -65,10 +73,10 @@ export class CachingGLTFLoader {
    * glTF. If the glTF has already been loaded, makes a clone of the cached
    * copy.
    */
-  async load(url) {
-    await this.preload(url);
+  async load(url: string, progressCallback: ProgressCallback|null = null) {
+    await this.preload(url, progressCallback);
 
-    const gltf = cloneGltf(await cache.get(url));
+    const gltf = cloneGltf(await cache.get(url)!);
     const model = gltf.scene ? gltf.scene : null;
 
     if (model != null) {
