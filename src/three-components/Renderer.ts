d316bc55e3b86f44fed2388951faa1cbb8fb39e0
Chris Joel <cdata@google.com>
2019-06-04 08:55:59 -0700

    Ensure we never attempt to draw from a negative coordinate in Renderer

--- src/three-components/Renderer.ts
+++ src/three-components/Renderer.ts
@@ -197,7 +197,7 @@ export default class Renderer extends EventDispatcher {
       context.drawImage(
           this.renderer.domElement,
           0,
-          this.canvas.height - heightDPR,
+          Math.max(0, this.canvas.height - heightDPR),
           widthDPR,
           heightDPR,
           0,
