53d52abde9f075d107c00afa113aacc887959d31
Chris Joel <cdata@google.com>
2019-06-11 12:03:05 -0700

    Add comment for enumerationDeserializer

--- src/conversions.ts
+++ src/conversions.ts
@@ -119,6 +119,26 @@ export const deserializeAngleToDeg = (angleString: string): number|null => {
   return null;
 };
 
+/**
+ * For our purposes, an enumeration is a fixed set of CSS-expression-compatible
+ * names. When serialized, a selected subset of the members may be specified as
+ * whitespace-separated strings. An enumeration deserializer is a function that
+ * parses a serialized subset of an enumeration and returns any members that are
+ * found as a Set.
+ *
+ * The following example will produce a deserializer for the days of the
+ * week:
+ *
+ * const deserializeDaysOfTheWeek = enumerationDeserializer([
+ *   'Monday',
+ *   'Tuesday',
+ *   'Wednesday',
+ *   'Thursday',
+ *   'Friday',
+ *   'Saturday',
+ *   'Sunday'
+ * ]);
+ */
 export const enumerationDeserializer = <T extends string>(allowedNames: T[]) =>
     (valueString: string): Set<T> => {
       try {
