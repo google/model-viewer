f869d9e11ea7bc20d296559d3d7eaee335405035
Emmett Lalish <elalish@google.com>
2019-09-12 15:31:47 -0700

    more cleanup

--- src/three-components/PMREMGenerator.ts
+++ src/three-components/PMREMGenerator.ts
@@ -19,24 +19,48 @@ import {environmentScene} from './EnvironmentScene.js';
 import {encodings, getDirectionChunk, texelIO} from './shader-chunk/common.glsl.js';
 import {bilinearCubeUVChunk} from './shader-chunk/cube_uv_reflection_fragment.glsl.js';
 
+/**
+ * This class generates a Prefiltered, Mipmapped Radiance Environment Map
+ * (PMREM) from a cubeMap environment texture. This allows different levels of
+ * blur to be quickly accessed based on material roughness. It is packed into a
+ * special CubeUV format that allows us to perform custom interpolation so that
+ * we can support nonlinear formats such as RGBE. Unlike a traditional mipmap
+ * chain, it only goes down to the LOD_MIN level (below), and then creates extra
+ * even more filtered 'mips' at the same LOD_MIN resolution, associated with
+ * higher roughness levels. In this way we maintain resolution to smoothly
+ * interpolate diffuse lighting while limiting sampling computation.
+ */
+
 const LOD_MIN = 4;
 const LOD_MAX = 8;
-const SIZE_MAX = Math.pow(2, LOD_MAX);
+// The roughness values associated with the extra mips. These must match
+// cube_uv_reflection_fragment.glsl.js.
 const EXTRA_LOD_ROUGHNESS = [0.22, 0.32, 0.5, 0.7, 1.0];
+// The standard deviations (radians) associated with the extra mips. These are
+// chosen to approximate a Trowbridge-Reitz distribution function times the
+// geometric shadowing function.
 const EXTRA_LOD_SIGMA = [0.12, 0.25, 0.35, 0.43, 0.5];
+const SIZE_MAX = Math.pow(2, LOD_MAX);
 const TOTAL_LODS = LOD_MAX - LOD_MIN + 1 + EXTRA_LOD_ROUGHNESS.length;
+
+// Number of standard deviations at which to cut off the discrete approximation.
+const STANDARD_DEVIATIONS = 3;
+// The maximum length of the blur for loop, chosen to equal the number needed
+// for GENERATED_SIGMA. Smaller sigmas will use fewer samples and exit early,
+// but not recompile the shader.
 const MAX_SAMPLES = 20;
+const GENERATED_SIGMA = 0.04;
 const DEFAULT_NEAR = 0.1;
 const DEFAULT_FAR = 100;
-const GENERATED_BLUR = 0.04;
 
 const $roughness = Symbol('roughness');
 const $sigma = Symbol('sigma');
-const $lodSize = Symbol('lodSize');
+const $sizeLod = Symbol('sizeLod');
 const $lodPlanes = Symbol('lodPlanes');
-const $blurShader = Symbol('blurShader');
+const $blurMaterial = Symbol('blurMaterial');
 const $flatCamera = Symbol('flatCamera');
 const $pingPongRenderTarget = Symbol('pingP$pingPongRenderTarget');
+
 const $sceneToCubeUV = Symbol('sceneToCubeUV');
 const $equirectangularToCubeUV = Symbol('equirectangularToCubeUV');
 const $createRenderTarget = Symbol('createRenderTarget');
@@ -44,23 +68,14 @@ const $applyPMREM = Symbol('applyPMREM');
 const $blur = Symbol('blur');
 const $halfBlur = Symbol('halfBlur');
 
-/**
- * This class generates a Prefiltered, Mipmapped Radiance Environment Map
- * (PMREM) from a cubeMap environment texture. This allows different levels of
- * blur to be quickly accessed based on material roughness. It is packed into a
- * special CubeUV format that allows us to perform custom interpolation so that
- * we can support nonlinear formats such as RGBE. Unlike a traditional mipmap
- * chain, it only goes down to the lodMin level (below), and then creates extra
- * even more filtered mips at the same lodMin resolution, associated with higher
- * roughness levels. In this way we maintain resolution to smoothly
- * interpolate diffuse lighting while limiting sampling computation.
- */
 export class PMREMGenerator {
-  private[$roughness]: Array < number >= [];
-  private[$sigma]: Array < number >= [];
-  private[$lodSize]: Array < number >= [];
-  private[$lodPlanes]: Array < BufferGeometry >= [];
-  private[$blurShader] = new BlurShader(MAX_SAMPLES);
+  // These arrays will each be TOTAL_LODS in length, each referring to a 'mip'.
+  private[$roughness]: Array<number> = [];
+  private[$sigma]: Array<number> = [];
+  private[$sizeLod]: Array<number> = [];
+  private[$lodPlanes]: Array<BufferGeometry> = [];
+
+  private[$blurMaterial] = new BlurMaterial(MAX_SAMPLES);
   private[$flatCamera] = new OrthographicCamera(0, 1, 0, 1, 0, 1);
   private[$pingPongRenderTarget]: WebGLRenderTarget;
 
@@ -68,7 +83,7 @@ export class PMREMGenerator {
     let lod = LOD_MAX;
     for (let i = 0; i < TOTAL_LODS; i++) {
       const sizeLod = Math.pow(2, lod);
-      this[$lodSize].push(sizeLod);
+      this[$sizeLod].push(sizeLod);
       let sigma = 1.0 / sizeLod;
       let roughness =
           (1 + Math.sqrt(1 + 4 * Math.PI * sizeLod)) / (2 * Math.PI * sizeLod);
@@ -85,42 +100,43 @@ export class PMREMGenerator {
       const min = -texelSize / 2;
       const max = 1 + texelSize / 2;
       const uv1 = [min, min, max, min, max, max, min, min, max, max, min, max];
-      const position = new Float32Array(3 * 3 * 2 * 6);
-      const uv = new Float32Array(2 * 3 * 2 * 6);
-      const faceIndex = new Float32Array(1 * 3 * 2 * 6);
 
-      for (let face = 0; face < 6; face++) {
+      const cubeFaces = 6;
+      const vertices = 6;
+      const positionSize = 3;
+      const uvSize = 2;
+      const faceIndexSize = 1;
+
+      const position = new Float32Array(positionSize * vertices * cubeFaces);
+      const uv = new Float32Array(uvSize * vertices * cubeFaces);
+      const faceIndex = new Float32Array(faceIndexSize * vertices * cubeFaces);
+
+      for (let face = 0; face < cubeFaces; face++) {
         const x = (face % 3) * 2 / 3 - 1;
         const y = face > 2 ? 0 : -1;
+        const coordinates = [
+          [x, y, 0],
+          [x + 2 / 3, y, 0],
+          [x + 2 / 3, y + 1, 0],
+          [x, y, 0],
+          [x + 2 / 3, y + 1, 0],
+          [x, y + 1, 0]
+        ];
         position.set(
-            [
-              x,
-              y,
-              0,
-              x + 2 / 3,
-              y,
-              0,
-              x + 2 / 3,
-              y + 1,
-              0,
-              x,
-              y,
-              0,
-              x + 2 / 3,
-              y + 1,
-              0,
-              x,
-              y + 1,
-              0
-            ],
-            3 * 3 * 2 * face);
-        uv.set(uv1, 2 * 3 * 2 * face);
-        faceIndex.fill(face, 3 * 2 * face, 3 * 2 * (face + 1));
+            ([] as number[]).concat(...coordinates),
+            positionSize * vertices * face);
+        uv.set(uv1, uvSize * vertices * face);
+        faceIndex.fill(
+            face,
+            faceIndexSize * vertices * face,
+            faceIndexSize * vertices * (face + 1));
       }
       const planes = new BufferGeometry();
-      planes.addAttribute('position', new BufferAttribute(position, 3));
-      planes.addAttribute('uv', new BufferAttribute(uv, 2));
-      planes.addAttribute('faceIndex', new BufferAttribute(faceIndex, 1));
+      planes.addAttribute(
+          'position', new BufferAttribute(position, positionSize));
+      planes.addAttribute('uv', new BufferAttribute(uv, uvSize));
+      planes.addAttribute(
+          'faceIndex', new BufferAttribute(faceIndex, faceIndexSize));
       this[$lodPlanes].push(planes);
 
       if (lod > LOD_MIN) {
@@ -134,7 +150,7 @@ export class PMREMGenerator {
 
     const cubeUVRenderTarget =
         this[$sceneToCubeUV](defaultScene, DEFAULT_NEAR, DEFAULT_FAR);
-    this[$blur](cubeUVRenderTarget, 0, 0, GENERATED_BLUR);
+    this[$blur](cubeUVRenderTarget, 0, 0, GENERATED_SIGMA);
     this[$applyPMREM](cubeUVRenderTarget);
 
     defaultScene.dispose();
@@ -229,8 +245,8 @@ export class PMREMGenerator {
     this[$pingPongRenderTarget] = this[$createRenderTarget](params);
 
     const scene = new Scene();
-    scene.add(new Mesh(this[$lodPlanes][0], this[$blurShader]));
-    const uniforms = this[$blurShader].uniforms;
+    scene.add(new Mesh(this[$lodPlanes][0], this[$blurMaterial]));
+    const uniforms = this[$blurMaterial].uniforms;
 
     uniforms.envMap.value = equirectangular;
     uniforms.copyEquirectangular.value = true;
@@ -265,6 +281,13 @@ export class PMREMGenerator {
     this[$pingPongRenderTarget].dispose();
   }
 
+  /**
+   * This is a two-pass Gaussian blur for a cubemap. Normally this is done
+   * vertically and horizontally, but this breaks down on a cube. Here we apply
+   * the blur latitudinally (around the poles), and then longitudinally (towards
+   * the poles) to approximate the orthogonally-separable blur. It is least
+   * accurate at the poles, but still does a decent job.
+   */
   private[$blur](
       cubeUVRenderTarget: WebGLRenderTarget, lodIn: number, lodOut: number,
       sigma: number) {
@@ -275,6 +298,7 @@ export class PMREMGenerator {
         lodOut,
         sigma,
         'latitudinal');
+
     this[$halfBlur](
         this[$pingPongRenderTarget],
         cubeUVRenderTarget,
@@ -293,50 +317,50 @@ export class PMREMGenerator {
     }
 
     const blurScene = new Scene();
-    blurScene.add(new Mesh(this[$lodPlanes][lodOut], this[$blurShader]));
-    const blurUniforms = this[$blurShader].uniforms;
+    blurScene.add(new Mesh(this[$lodPlanes][lodOut], this[$blurMaterial]));
+    const blurUniforms = this[$blurMaterial].uniforms;
 
-    const inputSize = this[$lodSize][lodIn];
-    const standardDeviations = 3;
-    const n = Math.ceil(
-        standardDeviations * standardDeviationRadians * inputSize * 2 /
+    const inputSize = this[$sizeLod][lodIn];
+    const samples = Math.ceil(
+        STANDARD_DEVIATIONS * standardDeviationRadians * inputSize * 2 /
         Math.PI);
-    // if (n > MAX_SAMPLES) {
-    console.log(
-        'StandardDeviationRadians, ',
-        standardDeviationRadians,
-        ', is too large and will clip, as it requested ',
-        n,
-        ' samples when the maximum is set to ',
-        MAX_SAMPLES);
-    // }
+
+    if (samples > MAX_SAMPLES) {
+      console.log(
+          'StandardDeviationRadians, ',
+          standardDeviationRadians,
+          ', is too large and will clip, as it requested ',
+          samples,
+          ' samples when the maximum is set to ',
+          MAX_SAMPLES);
+    }
+
     let weights = [];
     let sum = 0;
     for (let i = 0; i < MAX_SAMPLES; ++i) {
-      const x = standardDeviations * i / (n - 1);
+      const x = STANDARD_DEVIATIONS * i / (samples - 1);
       const weight = Math.exp(-x * x / 2);
       weights.push(weight);
       if (i == 0) {
         sum += weight;
-      } else if (i < n) {
+      } else if (i < samples) {
         sum += 2 * weight;
       }
     }
     weights = weights.map(w => w / sum);
-    console.log(weights);
 
     blurUniforms.envMap.value = targetIn.texture;
     blurUniforms.copyEquirectangular.value = false;
-    blurUniforms.samples.value = n;
+    blurUniforms.samples.value = samples;
     blurUniforms.weights.value = weights;
     blurUniforms.latitudinal.value = direction === 'latitudinal';
     blurUniforms.dTheta.value =
-        standardDeviationRadians * standardDeviations / (n - 1);
+        standardDeviationRadians * STANDARD_DEVIATIONS / (samples - 1);
     blurUniforms.mipInt.value = LOD_MAX - lodIn;
     blurUniforms.inputEncoding.value = encodings[targetIn.texture.encoding];
     blurUniforms.outputEncoding.value = encodings[targetIn.texture.encoding];
 
-    const outputSize = this[$lodSize][lodOut];
+    const outputSize = this[$sizeLod][lodOut];
     const x = 3 * Math.max(0, SIZE_MAX - 2 * outputSize);
     const y = (lodOut === 0 ? 0 : 2 * SIZE_MAX) +
         2 * outputSize *
@@ -350,7 +374,7 @@ export class PMREMGenerator {
 };
 
 
-class BlurShader extends RawShaderMaterial {
+class BlurMaterial extends RawShaderMaterial {
   constructor(maxSamples: number) {
     const weights = new Float32Array(maxSamples);
     const texelSize = new Vector2(1, 1);
