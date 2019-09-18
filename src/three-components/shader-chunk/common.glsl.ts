ec347686d06f73bc816baf3452b9a87012f9f1c1
Christopher Joel <cdata@google.com>
2019-09-18 09:45:39 -0700

    Add missing semi-colon

--- src/three-components/shader-chunk/common.glsl.ts
+++ src/three-components/shader-chunk/common.glsl.ts
@@ -15,7 +15,7 @@
 
 import {GammaEncoding, LinearEncoding, RGBDEncoding, RGBEEncoding, RGBM16Encoding, RGBM7Encoding, sRGBEncoding} from 'three';
 
-import {texelConversions} from './encodings_pars_fragment.glsl.js'
+import {texelConversions} from './encodings_pars_fragment.glsl.js';
 
 // These shader functions convert between the UV coordinates of a single face of
 // a cubemap, the 0-5 integer index of a cube face, and the direction vector for
