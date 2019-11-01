b1cdcb3639a8bdab1ec449f5fb0d8ffbe06793bc
Maas Lalani <maaslalani1@gmail.com>
2019-10-31 20:08:56 -0400

    Add License, Descriptive comment, and adjusted style guide to match other utility functions.

--- src/utilities/data-conversion.ts
+++ src/utilities/data-conversion.ts
@@ -1,28 +1,48 @@
-export function dataUrlToBlob(base64DataUrl: string) {
-  const sliceSize = 512;
-  const typeMatch = base64DataUrl.match(/data:(.*);/);
+/* @license
+ * Copyright 2019 Google LLC. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the 'License');
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an 'AS IS' BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-  if (!typeMatch) {
-    throw new Error(`${base64DataUrl} is not a valid data Url`);
-  }
+/**
+ * Converts a base64 string which represents a data url
+ * into a Blob of the same contents.
+ */
+export const dataUrlToBlob =
+  (base64DataUrl: string) : Blob => {
+    const sliceSize = 512;
+    const typeMatch = base64DataUrl.match(/data:(.*);/);
 
-  const type = typeMatch[1];
-  const base64 = base64DataUrl.replace(/data:image\/\w+;base64,/, '');
-
-  const byteCharacters = atob(base64);
-  const byteArrays = [];
-
-  for (let offset = 0; offset < byteCharacters.length; offset += sliceSize) {
-    const slice = byteCharacters.slice(offset, offset + sliceSize);
-
-    const byteNumbers = new Array(slice.length);
-    for (let i = 0; i < slice.length; i++) {
-      byteNumbers[i] = slice.charCodeAt(i);
+    if (!typeMatch) {
+      throw new Error(`${base64DataUrl} is not a valid data Url`);
     }
 
-    const byteArray = new Uint8Array(byteNumbers);
-    byteArrays.push(byteArray);
-  }
+    const type = typeMatch[1];
+    const base64 = base64DataUrl.replace(/data:image\/\w+;base64,/, '');
 
-  return new Blob(byteArrays, {type});
-}
+    const byteCharacters = atob(base64);
+    const byteArrays = [];
+
+    for (let offset = 0; offset < byteCharacters.length; offset += sliceSize) {
+      const slice = byteCharacters.slice(offset, offset + sliceSize);
+
+      const byteNumbers = new Array(slice.length);
+      for (let i = 0; i < slice.length; i++) {
+        byteNumbers[i] = slice.charCodeAt(i);
+      }
+
+      const byteArray = new Uint8Array(byteNumbers);
+      byteArrays.push(byteArray);
+    }
+
+    return new Blob(byteArrays, {type});
+  };
