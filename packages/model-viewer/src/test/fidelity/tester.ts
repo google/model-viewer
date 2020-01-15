9a2f299fc4c8374ce79bdbf175929f2481097b3a
Emmett Lalish <elalish@google.com>
2020-01-15 11:01:23 -0800

    added TS script

---
+++ packages/model-viewer/src/test/fidelity/tester.ts
@@ -0,0 +1,106 @@
+/* @license
+ * Copyright 2019 Google LLC. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {ModelViewerElement} from '../../model-viewer.js';
+
+const viewer = document.getElementById('loading-demo') as ModelViewerElement;
+
+let posterUrl = null;
+const a = document.createElement('a');
+const downloadButton = document.getElementById('download') as HTMLButtonElement;
+const displayButton = document.getElementById('display') as HTMLButtonElement;
+downloadButton.disabled = true;
+displayButton.disabled = true;
+let orbitString = '';
+
+async function createPoster() {
+  const orbit = viewer.getCameraOrbit();
+  orbitString = `${orbit.theta}rad ${orbit.phi}rad auto`;
+  viewer.fieldOfView = 'auto';
+  viewer.jumpCameraToGoal();
+  await new Promise(resolve => requestAnimationFrame(() => resolve()));
+  URL.revokeObjectURL(posterUrl);
+  const blob = await viewer.toBlob({mimeType: 'image/jpeg', idealAspect: true});
+  posterUrl = URL.createObjectURL(blob);
+  downloadButton.disabled = false;
+  displayButton.disabled = false;
+}
+
+function reloadScene() {
+  viewer.poster = posterUrl;
+  viewer.reveal = 'interaction';
+  viewer.cameraOrbit = orbitString;
+  const src = viewer.src;
+  viewer.src = null;
+  viewer.src = src;
+}
+
+function downloadPoster() {
+  a.href = posterUrl;
+  a.download = 'poster.jpg';
+  a.click();
+}
+
+(() => {
+  viewer.addEventListener('dragover', (event) => {
+    event.preventDefault();
+  });
+  viewer.addEventListener('drop', (event) => {
+    event.preventDefault();
+    const file = event.dataTransfer.files[0];
+    const filename = file.name.toLowerCase();
+    const target = event.target as ModelViewerElement;
+    if (filename.match(/\.(gltf|glb)$/)) {
+      target.src = URL.createObjectURL(file);
+    } else if (filename.match(/\.(hdr)$/)) {
+      target.skyboxImage = URL.createObjectURL(file) + '#.hdr';
+    } else if (filename.match(/\.(png|jpg)$/)) {
+      target.skyboxImage = URL.createObjectURL(file);
+    }
+  });
+  viewer.addEventListener('error', (event) => {
+    console.error((event as any).detail);
+    viewer.src = 'assets/Astronaut.glb';
+  });
+
+  document.getElementById('src')!.addEventListener('input', (event) => {
+    viewer.src = (event.target as HTMLInputElement).value;
+  });
+
+  document.getElementById('skyboxImage')!.addEventListener('input', (event) => {
+    viewer.skyboxImage = (event.target as HTMLInputElement).value;
+  });
+
+  ['exposure', 'shadowIntensity'].forEach((property) => {
+    const input = document.getElementById(`${property}`) as HTMLInputElement;
+    const output =
+        document.getElementById(`${property}Value`) as HTMLInputElement;
+    input.addEventListener('input', (event) => {
+      output.value = (event.target as HTMLInputElement).value;
+      viewer[property] = parseFloat(output.value);
+    });
+    output.addEventListener('input', (event) => {
+      input.value = (event.target as HTMLInputElement).value;
+      viewer[property] = parseFloat(output.value);
+    });
+  });
+
+  ['autoRotate'].forEach((property) => {
+    const checkbox = document.getElementById(`${property}`) as HTMLInputElement;
+    checkbox.addEventListener('change', (event) => {
+      viewer[property] = checkbox.checked;
+    });
+  });
+})();
