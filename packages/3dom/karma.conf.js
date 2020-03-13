7522d022df0d010d4f0c480e0388443c1beeb239
Chris Joel <cdata@google.com>
2020-03-13 11:33:34 -0700

    Force a unique BS tunnel identifier

--- packages/3dom/karma.conf.js
+++ packages/3dom/karma.conf.js
@@ -13,6 +13,50 @@
  * limitations under the License.
  */
 
+const buildIdentifier = process.env.BROWSER_STACK_BUILD_NAME || `${Date.now()}`;
+
+// This terrible hack needed in order to specify a custom BrowserStack
+// tunnel identifier. Failures to do so results in overlapping tunnels when
+// builds run in parallel:
+{
+  // Patch global require so that we can intercept a resolved module by name. It
+  // isn't sufficient to require the module ourselves because it is a transitive
+  // dependency and we might not resolve the correct version:
+  const Module = module.constructor;
+  const require = Module.prototype.require;
+  Module.prototype.require = function(...args) {
+    const resolvedModule = require.apply(this, args);
+    if (args[0] === 'browserstack-local') {
+      const {Local} = resolvedModule;
+      // Annoyingly, browserstack-local populates methods using assignment on
+      // the instance rather than decorating the prototype, so we have to wrap
+      // the constructor in order to patch anything:
+      // @see https://github.com/browserstack/browserstack-local-nodejs/blob/d238484416e7ea6dfb51aede7d84d09339a8032a/lib/Local.js#L28
+      const WrappedLocal = function(...args) {
+        // Create an instance of the canonical class and patch its method post
+        // hoc before it is handed off to the invoking user:
+        const local = new Local(...args);
+        const start = local.start;
+        local.start = function(...args) {
+          const config = args[0];
+          // If the config is lacking a specified identifier for the tunnel,
+          // make sure to populate it with the one we want:
+          if (config && config.localIdentifier == null) {
+            console.warn(
+                'Patching BrowserStack tunnel configuration to specify unique ID:',
+                buildIdentifier);
+            config.localIdentifier = buildIdentifier;
+          }
+          return start.apply(this, args);
+        };
+        return local;
+      };
+      resolvedModule.Local = WrappedLocal;
+    }
+    return resolvedModule;
+  };
+}
+
 module.exports = function(config) {
   // @see http://karma-runner.github.io/4.0/config/configuration-file.html
   config.set({
@@ -81,11 +125,12 @@ module.exports = function(config) {
       // the test-specific browser ID and append it to our URL:
       // @see https://github.com/karma-runner/karma-browserstack-launcher/blob/76dbfd0db6db46f4f85012cfe3c1f4c3accd2e44/index.js#L143
       if (args[2] != null && args[2].url === 'http://bs-local.com:9876') {
-        console.warn('PATCHING URL TO ADD ID');
         const config = args[0];
         const browser = args[2];
         const query = config.url.split('?')[1];
         browser.url = `${browser.url}?${query}`;
+
+        console.warn('Patching test URL to add Karma ID:', browser.url);
       }
       return assign.apply(this, args);
     };
@@ -161,12 +206,14 @@ module.exports = function(config) {
       },
     };
 
+
     config.set({
       browserStack: {
         idleTimeout: 600,
         name: '3DOM Unit Tests',
         project: '3DOM',
-        build: process.env.BROWSER_STACK_BUILD_NAME || `${Date.now()}`,
+        build: buildIdentifier,
+        tunnelIdentifier: buildIdentifier
       },
 
       reporters: ['BrowserStack', 'mocha'],
