5b3131e52b98aefe1e4f3e927c9813eda36a2f94
Jiaxin Sun <mrfrog@google.com>
2020-08-18 17:36:49 -0400

    setup basic spec template

--- packages/model-viewer/src/test/model-viewer-spec.ts
+++ packages/model-viewer/src/test/model-viewer-spec.ts
@@ -1,5 +1,38 @@
-const expect = chai.expect;
+import {ModelViewerElement} from '../model-viewer';
+import {Constructor} from '../utilities';
 
-suite('test included', () => {test('included? ', () => {
-                         expect(4).to.be.equal(5);
-                       })})
+import {BasicSpecTemplate} from './templates';
+
+// const expect = chai.expect;
+
+suite.only('ModelViewerElement', () => {
+  // global variable
+  let nextId: number = 0;
+  let tagName: string;
+  let ModelViewer: Constructor<ModelViewerElement>;
+
+  setup(() => {
+    tagName = `model-viewer-${nextId++}`;
+    ModelViewer = class extends ModelViewerElement {
+      static get is() {
+        return tagName;
+      }
+    }
+    // not sure what does this do
+    customElements.define(tagName, ModelViewer);
+  })
+
+  BasicSpecTemplate(() => ModelViewer, () => tagName);
+  // tear down
+
+  // basic template (optional)
+
+  suite(
+      'Fidelity Test',
+      () => {
+          // setup-fidleity test,  read config file here
+
+          // test each scenarios
+
+      })
+})
