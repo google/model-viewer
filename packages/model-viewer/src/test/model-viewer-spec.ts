2efe550ef6e8ca03c4f9bfbd168704fc2e7d9e76
Jiaxin Sun <mrfrog@google.com>
2020-08-20 01:26:08 -0400

    got working

--- packages/model-viewer/src/test/model-viewer-spec.ts
+++ packages/model-viewer/src/test/model-viewer-spec.ts
@@ -1,17 +1,17 @@
 // why do we have to specify the type to be .js? something about mocha?
 import {ModelViewerElement} from '../model-viewer.js';
 import {Constructor} from '../utilities.js';
-import {assetPath, CONFIG_PATH, waitForEvent} from './helpers.js';
+import {assetPath, waitForEvent} from './helpers.js';
 import {BasicSpecTemplate} from './templates.js';
 
 const expect = chai.expect;
 
-// where does the base comefrom?
 interface scenarioConfig {
   name: string, model: string, lighting: string,
       dimensions: {width: number, height: number},
       target: {x: number, y: number, z: number},
-      orbit: {theta: number, phi: number, radius: number}, verticalFoV: number
+      orbit: {theta: number, phi: number, radius: number}, verticalFoV: number,
+      renderSkybox: boolean
 }
 
 interface FidelityResult {
@@ -19,170 +19,191 @@ interface FidelityResult {
   transparentPixelCount: number;
 }
 
-const setupModelViewer =
-    async (
-        modelViewer: ModelViewerElement,
-        config:
-            any) => {
-  modelViewer.style.width = `${config.dimensions.width}px`;
-  modelViewer.style.height = `${config.dimensions.height}px`;
+const SCENARIOS: Array<scenarioConfig> =
+    [
+      {
+        name: 'khronos-MetalRoughSpheres',
+        model:
+            'models/glTF-Sample-Models/2.0/MetalRoughSpheres/glTF/MetalRoughSpheres.gltf',
+        lighting: 'environments/lightroom_14b.hdr',
+        dimensions: {width: 100, height: 100},
+        target: {x: 0, y: 0, z: 0},
+        orbit: {theta: 0, phi: 90, radius: 12},
+        verticalFoV: 45,
+        renderSkybox: true
+      },
+      {
+        name: 'khronos-MetalRoughSpheres-HDR',
+        model:
+            'models/glTF-Sample-Models/2.0/MetalRoughSpheres/glTF/MetalRoughSpheres.gltf',
+        lighting: 'environments/spruit_sunrise_1k_HDR.hdr',
+        dimensions: {width: 100, height: 100},
+        target: {x: 0, y: 0, z: 0},
+        orbit: {theta: 0, phi: 90, radius: 12},
+        verticalFoV: 45,
+        renderSkybox: true
+      },
+      {
+        name: 'khronos-MetalRoughSpheres-LDR',
+        model:
+            'models/glTF-Sample-Models/2.0/MetalRoughSpheres/glTF/MetalRoughSpheres.gltf',
+        lighting: 'environments/spruit_sunrise_1k_LDR.jpg',
+        dimensions: {width: 100, height: 100},
+        target: {x: 0, y: 0, z: 0},
+        orbit: {theta: 0, phi: 90, radius: 12},
+        verticalFoV: 45,
+        renderSkybox: true
+      }
+    ]
 
-  modelViewer.style.backgroundColor = 'rgba(255,255,255,0)';
+    const setupModelViewer =
+        async (
+            modelViewer: ModelViewerElement,
+            config:
+                any) => {
+      modelViewer.style.width = `${config.dimensions.width}px`;
+      modelViewer.style.height = `${config.dimensions.height}px`;
 
-  modelViewer.src = assetPath(config.model);
+      modelViewer.style.backgroundColor = 'rgba(255,255,255,0)';
 
-  const lightingPath = assetPath(config.lighting);
-  modelViewer.environmentImage = lightingPath;
-  modelViewer.skyboxImage = null;
+      const {model, lighting, orbit, renderSkybox} = config;
+      modelViewer.src = assetPath(model);
 
-  const {theta, phi, radius} = config.orbit;
-  modelViewer.minCameraOrbit = `auto auto ${radius}m`;
-  modelViewer.maxCameraOrbit = `auto auto ${radius}m`;
-  modelViewer.cameraOrbit = `${theta}deg ${phi}deg ${radius}m`;
-  const {x, y, z} = config.target;
-  modelViewer.cameraTarget = `${x}m ${y}m ${z}m`;
-  modelViewer.fieldOfView = `${config.verticalFoV}deg`;
+      const lightingPath = assetPath(lighting);
+      modelViewer.environmentImage = lightingPath;
+      modelViewer.skyboxImage = renderSkybox ? lightingPath : null;
 
-  await waitForEvent(modelViewer, 'poster-dismissed');
-}
+      const {theta, phi, radius} = orbit;
+      modelViewer.minCameraOrbit = `auto auto ${radius}m`;
+      modelViewer.maxCameraOrbit = `auto auto ${radius}m`;
+      modelViewer.cameraOrbit = `${theta}deg ${phi}deg ${radius}m`;
+      const {x, y, z} = config.target;
+      modelViewer.cameraTarget = `${x}m ${y}m ${z}m`;
+      modelViewer.fieldOfView = `${config.verticalFoV}deg`;
 
-                    async function captureScreenshot(blob: Blob):
-                        Promise<HTMLCanvasElement> {
-                          const image =
-                              await new Promise<HTMLImageElement>((resolve) => {
-                                const image = new Image();
-                                const url = URL.createObjectURL(blob);
-                                image.src = url;
-                                image.onload = () => {
-                                  resolve(image);
-                                }
-                              })
+      await waitForEvent(modelViewer, 'poster-dismissed');
+    }
 
-                          const fiddleCanvas = document.createElement('canvas');
-                          fiddleCanvas.width = image.width;
-                          fiddleCanvas.height = image.height;
-                          fiddleCanvas.getContext('2d')?.drawImage(
-                              image, 0, 0, image.width, image.height);
+                        async function captureScreenshot(blob: Blob):
+                            Promise<HTMLCanvasElement> {
+                              const image = await new Promise<HTMLImageElement>(
+                                  (resolve) => {
+                                    const image = new Image();
+                                    const url = URL.createObjectURL(blob);
+                                    image.src = url;
+                                    image.onload = () => {
+                                      resolve(image);
+                                    }
+                                  })
 
-                          return Promise.resolve(fiddleCanvas);
-                        }
+                              const fiddleCanvas =
+                                  document.createElement('canvas');
+                              fiddleCanvas.width = image.width;
+                              fiddleCanvas.height = image.height;
+                              fiddleCanvas.getContext('2d')?.drawImage(
+                                  image, 0, 0, image.width, image.height);
 
-                    // TODO(sun765): this only test whether the screenshot is
-                    // colorless or not. Replace this with more robust test
-                    // later.
-                    function testFidelity(screenshotCanvas: HTMLCanvasElement):
-                        FidelityResult {
-                          let colorlessPixelCount = 0;
-                          let transparentPixelCount = 0;
-                          for (let row = 0; row < screenshotCanvas.height;
-                               row++) {
-                            for (let col = 0; col < screenshotCanvas.width;
-                                 col++) {
-                              const pixelData =
-                                  screenshotCanvas.getContext('2d')
-                                      ?.getImageData(col, row, 1, 1)
-                                      .data;
-                              let isWhite = true;
-                              let isBlack = true;
+                              return Promise.resolve(fiddleCanvas);
+                            }
 
-                              if (pixelData![3] === 0)
-                                transparentPixelCount++;
+                        // TODO(sun765): this only test whether the screenshot
+                        // is colorless or not. Replace this with more robust
+                        // test later.
+                        function testFidelity(
+                            screenshotCanvas: HTMLCanvasElement):
+                            FidelityResult {
+                              let colorlessPixelCount = 0;
+                              let transparentPixelCount = 0;
+                              for (let row = 0; row < screenshotCanvas.height;
+                                   row++) {
+                                for (let col = 0; col < screenshotCanvas.width;
+                                     col++) {
+                                  const pixelData =
+                                      screenshotCanvas.getContext('2d')
+                                          ?.getImageData(col, row, 1, 1)
+                                          .data;
+                                  let isWhite = true;
+                                  let isBlack = true;
 
-                              for (let i = 0; i < 3; i++) {
-                                const colorComponent = pixelData![i];
-                                if (colorComponent != 255) {
-                                  isWhite = false;
-                                }
-                                if (colorComponent != 0) {
-                                  isBlack = false;
+                                  if (pixelData![3] === 0)
+                                    transparentPixelCount++;
+
+                                  for (let i = 0; i < 3; i++) {
+                                    const colorComponent = pixelData![i];
+                                    if (colorComponent != 255) {
+                                      isWhite = false;
+                                    }
+                                    if (colorComponent != 0) {
+                                      isBlack = false;
+                                    }
+                                  }
+
+                                  if (isWhite || isBlack) {
+                                    colorlessPixelCount++;
+                                  }
                                 }
                               }
 
-                              if (isWhite || isBlack) {
-                                colorlessPixelCount++;
+                              return {
+                                colorlessPixelCount,
+                                transparentPixelCount
+                              };
+                            }
+
+                        suite.only('ModelViewerElement', () => {
+                          let nextId: number = 0;
+                          let tagName: string;
+                          let ModelViewer: Constructor<ModelViewerElement>;
+
+                          setup(() => {
+                            tagName = `model-viewer-${nextId++}`;
+                            ModelViewer = class extends ModelViewerElement {
+                              static get is() {
+                                return tagName;
                               }
                             }
-                          }
+                            customElements.define(tagName, ModelViewer);
+                          })
 
-                          return {colorlessPixelCount, transparentPixelCount};
-                        }
+                          BasicSpecTemplate(() => ModelViewer, () => tagName);
 
-                    suite.only('ModelViewerElement', () => {
-                      // global variable
-                      let nextId: number = 0;
-                      let tagName: string;
-                      let ModelViewer: Constructor<ModelViewerElement>;
+                          suite('Fidelity Test', () => {
+                            SCENARIOS.forEach((scenario) => {
+                              suite(`test ${scenario.name}`, () => {
+                                let element: ModelViewerElement;
 
-                      setup(() => {
-                        tagName = `model-viewer-${nextId++}`;
-                        ModelViewer = class extends ModelViewerElement {
-                          static get is() {
-                            return tagName;
-                          }
-                        }
-                        // not sure what does this do
-                        customElements.define(tagName, ModelViewer);
-                      })
+                                setup(async () => {
+                                  element = new ModelViewerElement();
+                                  document.body.insertBefore(
+                                      element, document.body.firstChild);
+                                  await setupModelViewer(element, scenario);
+                                });
 
-                      BasicSpecTemplate(() => ModelViewer, () => tagName);
+                                teardown(() => {
+                                  if (element.parentNode != null) {
+                                    element.parentNode.removeChild(element);
+                                  }
+                                });
 
-                      suite('Fidelity Test', () => {
-                        let element: ModelViewerElement;
-                        let scenarios: Array<scenarioConfig>;
+                                test(scenario.name, async () => {
+                                  const blob = await element.toBlob();
+                                  const screenshotCanvas =
+                                      await captureScreenshot(blob);
+                                  const result = testFidelity(screenshotCanvas);
+                                  const {
+                                    transparentPixelCount,
+                                    colorlessPixelCount
+                                  } = result;
+                                  const imagePixelCount =
+                                      screenshotCanvas.width *
+                                      screenshotCanvas.height;
 
-                        setup(async () => {
-                          element = new ModelViewerElement();
-                          document.body.insertBefore(
-                              element, document.body.firstChild);
-
-                          scenarios = (await (await fetch(CONFIG_PATH!)).json())
-                                          .scenarios;
+                                  expect(transparentPixelCount)
+                                      .to.be.below(imagePixelCount);
+                                  expect(colorlessPixelCount)
+                                      .to.be.below(imagePixelCount);
+                                });
+                              });
+                            });
+                          });
                         })
-
-                        teardown(() => {
-                          // clear whatever in the setup file
-                          if (element.parentNode != null) {
-                            element.parentNode.removeChild(element);
-                          }
-
-                          console.log(scenarios);
-                        })
-
-                        // TODO: change this to test all scenarios
-    suite('Check all scenarions', () => {
-
-      setup(async () => {
-      await setupModelViewer(element, scenarios[0]);
-      })
-
-    test('test one scenario first', async () => {
-      // capture screenshot;
-      const blob = await element.toBlob();
-      const screenshotCanvas = await captureScreenshot(blob);
-      const testResult = testFidelity(screenshotCanvas);
-      const {colorlessPixelCount, transparentPixelCount} = testResult;
-
-      const imagePixels = screenshotCanvas.width * screenshotCanvas.height;
-      console.log(
-          'colorless pixels rate' +
-          (colorlessPixelCount / imagePixels).toFixed(2));
-      console.log(
-          'transparent pixel rate' +
-          (transparentPixelCount / imagePixels).toFixed(2));
-      expect(colorlessPixelCount).to.be.below(imagePixels);
-      expect(transparentPixelCount).to.be.below(imagePixels);
-      // download the image
-      /*
-      const link = document.createElement('a');
-      link.download = 'golden.png';
-      link.href = screenshotCanvas.toDataURL();
-      link.click();
-      */
-    })
-
-      teardown(() => {
-        // clear all setup for model-viewer
-      })
-    })
-                      })
-                    })
