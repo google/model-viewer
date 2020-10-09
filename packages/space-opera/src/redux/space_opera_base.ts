0c5a860ce2ff9a345fea5822be477024cc6ae627
Christopher George <chrisgeorge@google.com>
2020-10-09 06:53:20 -1000

    add redux devtools (#1615)
    * add redux devtools
    
    * update to recent version of devtools

--- packages/space-opera/src/redux/space_opera_base.ts
+++ packages/space-opera/src/redux/space_opera_base.ts
@@ -97,8 +97,18 @@ function makeRootReducer() {
   };
 }
 
+/** Setup devtools */
+declare global {
+  interface Window {
+    __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof Redux.compose;
+  }
+}
+const composeEnhancers =
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || Redux.compose
+
 /** Global Redux store. */
-export const reduxStore = Redux.createStore(makeRootReducer());
+export const reduxStore =
+    Redux.createStore(makeRootReducer(), composeEnhancers());
 
 /**
  * A synchronous mutator of fixed paylod type.
