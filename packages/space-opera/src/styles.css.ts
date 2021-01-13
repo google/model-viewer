42953ac3a8e65d988295c91d09647d67a67e4a74
Christopher George <chrisgeorge@google.com>
2021-01-13 15:51:22 -0500

    fix glitchy toast on mobile

--- packages/space-opera/src/styles.css.ts
+++ packages/space-opera/src/styles.css.ts
@@ -286,8 +286,8 @@ export const toastStyles: CSSResult = css`
 
 #snackbar-mobile.show {
   visibility: visible;
-  -webkit-animation: fadein 0.5s, fadeout 0.5s 5.5s;
-  animation: fadein 0.5s, fadeout 0.5s 5.5s;
+  -webkit-animation: fadein 0.5s, fadeout 0.5s 6.5s;
+  animation: fadein 0.5s, fadeout 0.5s 6.5s;
 }
 
 @-webkit-keyframes fadein {
