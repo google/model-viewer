123b8acd2f41049b521fd401114ef3146631cb41
Christopher George <chrisgeorge@google.com>
2021-01-26 15:32:05 -0500

    remove q

--- packages/space-opera/src/components/best_practices/constants.ts
+++ packages/space-opera/src/components/best_practices/constants.ts
@@ -46,7 +46,7 @@ const onProgress = (event) => {
   } else {
     progressBar.classList.remove('hide');
     if (event.detail.totalProgress === 0) {
-      event.target.querySelector('.center-pre-prompt')?.classList.add('hide');
+      event.target.querySelector('.center-pre-prompt').classList.add('hide');
     }
   }
 };
