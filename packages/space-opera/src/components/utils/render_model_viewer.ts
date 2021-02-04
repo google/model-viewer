9beafcb5c0b87647d40521db3fb0282320243468
Riccardo Arena <riccardo@puntoquindici.it>
2021-02-04 23:08:11 +0100

    Add basic error handling in space-opera viewer preview. (#1972)

--- packages/space-opera/src/components/utils/render_model_viewer.ts
+++ packages/space-opera/src/components/utils/render_model_viewer.ts
@@ -32,6 +32,7 @@ export interface ModelViewerEventHandlers {
   readonly play?: () => void;
   readonly pause?: () => void;
   readonly click?: (event: MouseEvent) => void;
+  readonly error?: (details: CustomEvent) => void;
 }
 
 /**
@@ -74,6 +75,7 @@ export function renderModelViewer(
         @play=${eventHandlers?.play}
         @pause=${eventHandlers?.pause}
         @click=${eventHandlers?.click}
+        @error=${eventHandlers?.error}
       >
       ${childElements}
       </model-viewer>`;
