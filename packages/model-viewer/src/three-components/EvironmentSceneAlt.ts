5dd28f53bd99c2bea411267c1fa81c42c374be5c
thomastgt <thomas.huang@target.com>
2021-04-05 22:36:01 -0500

    adjust +X to bring the lighting value to conformity

--- packages/model-viewer/src/three-components/EvironmentSceneAlt.ts
+++ packages/model-viewer/src/three-components/EvironmentSceneAlt.ts
@@ -88,7 +88,7 @@ export default class EnvironmentSceneAlt extends Scene {
 
 
     // +x
-    const light3 = new Mesh(geometry, this.createAreaLightMaterial(16));
+    const light3 = new Mesh(geometry, this.createAreaLightMaterial(23));
     light3.position.set(14.0, 12.0, 0.0);
     light3.scale.set(0.1, 5.0, 5.0);
     this.add(light3);
