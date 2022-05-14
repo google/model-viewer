84726efd3e5f16616245537fb99f2fe136e95704
e111077 <5981958+e111077@users.noreply.github.com>
2022-05-13 18:25:50 -0700

    fix test formatting issues

--- packages/space-opera/src/test/snippet_viewer/snippet_viewer_test.ts
+++ packages/space-opera/src/test/snippet_viewer/snippet_viewer_test.ts
@@ -33,31 +33,22 @@ describe('snippet viewer test', () => {
   });
 
   it('formatted HTML strips Lit comments', async () => {
+    // clang-format off
+
     // Pulled from real DOM of astronaut example.
-    snippetViewer.renderedSnippet = html`<!--?lit$343342268$--><model-viewer
-        bounds="tight"
-        enable-pan=""
-        src="Astronaut.glb"
-        ar=""
-        ar-modes="webxr scene-viewer quick-look"
-        camera-controls=""
-        environment-image="neutral"
-        poster="poster.webp"
-        shadow-intensity="1"
-        ar-status="not-presenting"
-      >
-        <!--?lit$343342268$--><!---->
-        <div class="progress-bar hide" slot="progress-bar">
-          <div class="update-bar"></div>
-        </div>
-        <!----><!---->
-        <button slot="ar-button" id="ar-button">View in your space</button
-        ><!----><!---->
-        <div id="ar-prompt">
-          <img />
-        </div>
-        <!---->
-      </model-viewer>`;
+    snippetViewer.renderedSnippet = html`<!--?lit$343342268$--><model-viewer bounds="tight" enable-pan="" src="Astronaut.glb" ar="" ar-modes="webxr scene-viewer quick-look" camera-controls="" environment-image="neutral" poster="poster.webp" shadow-intensity="1" ar-status="not-presenting">
+    <!--?lit$128424273$--><!---->
+<div class="progress-bar hide" slot="progress-bar">
+<div class="update-bar"></div>
+</div><!----><!---->
+<button slot="ar-button" id="ar-button">
+View in your space
+</button><!----><!---->
+<div id="ar-prompt">
+<img>
+</div><!---->
+    </model-viewer>`;
+    // clang-format on
 
     const goldenFormattedHTML = `<model-viewer bounds="tight" enable-pan src="Astronaut.glb" ar ar-modes="webxr scene-viewer quick-look" camera-controls environment-image="neutral" poster="poster.webp" shadow-intensity="1">
     <div class="progress-bar hide" slot="progress-bar">
@@ -69,48 +60,30 @@ describe('snippet viewer test', () => {
     <div id="ar-prompt">
         <img>
     </div>
-</model-viewer>
-`;
+</model-viewer>`;
 
     await snippetViewer.updateComplete;
     expect(snippetViewer.formattedHtml).toBe(goldenFormattedHTML);
   });
 
   it('formatted HTML does not greedily strip comments', async () => {
+    // clang-format off
+
     // Pulled from real DOM of astronaut example with a hotspot.
     // hotspot <button> is beteen two comments on the same line
-    snippetViewer.renderedSnippet = html`<!--?lit$99301137$--><model-viewer
-        bounds="tight"
-        enable-pan=""
-        src="Astronaut.glb"
-        ar=""
-        ar-modes="webxr scene-viewer quick-look"
-        camera-controls=""
-        environment-image="neutral"
-        poster="poster.webp"
-        shadow-intensity="1"
-        ar-status="not-presenting"
-      >
-        <!--?lit$99301137$--><!----><button
-          class="Hotspot"
-          slot="hotspot-1"
-          data-position="-0.043973778464142396m 1.2075171453793048m 0.29653766978435936m"
-          data-normal="-0.4260645307016329m -0.06968452861538316m 0.9020050344369756m"
-          data-visibility-attribute="visible"
-        >
-          <div class="HotspotAnnotation">asdf</div></button
-        ><!----><!---->
-        <div class="progress-bar hide" slot="progress-bar">
-          <div class="update-bar"></div>
-        </div>
-        <!----><!---->
-        <button slot="ar-button" id="ar-button">View in your space</button
-        ><!----><!---->
-        <div id="ar-prompt">
-          <img />
-        </div>
-        <!---->
-      </model-viewer>`;
+    snippetViewer.renderedSnippet = html`<!--?lit$128424273$--><model-viewer bounds="tight" enable-pan="" src="Astronaut.glb" ar="" ar-modes="webxr scene-viewer quick-look" camera-controls="" environment-image="neutral" poster="poster.webp" shadow-intensity="1" ar-status="not-presenting">
+    <!--?lit$128424273$--><!----><button class="Hotspot" slot="hotspot-1" data-position="-0.043973778464142396m 1.2075171453793048m 0.29653766978435936m" data-normal="-0.4260645307016329m -0.06968452861538316m 0.9020050344369756m" data-visibility-attribute="visible"><div class="HotspotAnnotation">asdf</div></button><!----><!---->
+<div class="progress-bar hide" slot="progress-bar">
+<div class="update-bar"></div>
+</div><!----><!---->
+<button slot="ar-button" id="ar-button">
+View in your space
+</button><!----><!---->
+<div id="ar-prompt">
+<img>
+</div><!---->
+    </model-viewer>`;
+    // clang-format on
 
     const goldenFormattedHTML = `<model-viewer bounds="tight" enable-pan src="Astronaut.glb" ar ar-modes="webxr scene-viewer quick-look" camera-controls environment-image="neutral" poster="poster.webp" shadow-intensity="1">
     <button class="Hotspot" slot="hotspot-1" data-position="-0.043973778464142396m 1.2075171453793048m 0.29653766978435936m" data-normal="-0.4260645307016329m -0.06968452861538316m 0.9020050344369756m" data-visibility-attribute="visible">
@@ -125,8 +98,7 @@ describe('snippet viewer test', () => {
     <div id="ar-prompt">
         <img>
     </div>
-</model-viewer>
-`;
+</model-viewer>`;
 
     await snippetViewer.updateComplete;
     expect(snippetViewer.formattedHtml).toBe(goldenFormattedHTML);
