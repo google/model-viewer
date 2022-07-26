88b12bf73fbc5be5eecfbd4893de1fd2079758d5
Brian Cairns <bcairns@logitech.com>
2022-07-26 17:32:03 -0600

    apply ceil() to canvas (prevent potential resampling from fractional pixel dimensions) (#3555)
    * apply floor() to canvas and container (prevent potential upsampling from fractional pixel dimensions)
    
    (https://github.com/google/model-viewer/issues/3554)
    
    * Use Math.floor() in render() as well
    
    * switch to using ceil(), and move overflow:hidden to .container
    
    (this feature branch is now HORRIBLY named)

--- packages/model-viewer/src/three-components/Renderer.ts
+++ packages/model-viewer/src/three-components/Renderer.ts
@@ -204,13 +204,13 @@ export class Renderer extends EventDispatcher {
     this.dpr = dpr;
 
     if (this.canRender) {
-      this.threeRenderer.setSize(width * dpr, height * dpr, false);
+      this.threeRenderer.setSize(Math.ceil(width * dpr), Math.ceil(height * dpr), false);
     }
 
     // Expand the canvas size to make up for shrinking the viewport.
     const scale = this.scaleFactor;
-    const widthCSS = width / scale;
-    const heightCSS = height / scale;
+    const widthCSS = Math.ceil(width / scale);
+    const heightCSS = Math.ceil(height / scale);
     // The canvas element must by styled outside of three due to the offscreen
     // canvas not being directly stylable.
     this.canvas3D.style.width = `${widthCSS}px`;
@@ -221,8 +221,8 @@ export class Renderer extends EventDispatcher {
     // and only the portion that is shown is copied over.
     for (const scene of this.scenes) {
       const {canvas} = scene;
-      canvas.width = Math.round(width * dpr);
-      canvas.height = Math.round(height * dpr);
+      canvas.width = Math.ceil(width * dpr);
+      canvas.height = Math.ceil(height * dpr);
       canvas.style.width = `${widthCSS}px`;
       canvas.style.height = `${heightCSS}px`;
       scene.queueRender();
@@ -246,8 +246,8 @@ export class Renderer extends EventDispatcher {
     this.avgFrameDuration =
         (HIGH_FRAME_DURATION_MS + LOW_FRAME_DURATION_MS) / 2;
 
-    const width = this.width / scale;
-    const height = this.height / scale;
+    const width = Math.ceil(this.width / scale);
+    const height = Math.ceil(this.height / scale);
 
     this.canvas3D.style.width = `${width}px`;
     this.canvas3D.style.height = `${height}px`;
@@ -283,8 +283,8 @@ export class Renderer extends EventDispatcher {
     const {canvas} = scene;
     const scale = this.scaleFactor;
 
-    canvas.width = Math.round(this.width * this.dpr);
-    canvas.height = Math.round(this.height * this.dpr);
+    canvas.width = Math.ceil(this.width * this.dpr);
+    canvas.height = Math.ceil(this.height * this.dpr);
 
     canvas.style.width = `${this.width / scale}px`;
     canvas.style.height = `${this.height / scale}px`;
@@ -482,7 +482,7 @@ export class Renderer extends EventDispatcher {
       // clearing the depth from a different buffer
       this.threeRenderer.setRenderTarget(null);
       this.threeRenderer.setViewport(
-          0, Math.floor(this.height * dpr) - height, width, height);
+          0, Math.ceil(this.height * dpr) - height, width, height);
       this.threeRenderer.render(scene, scene.camera);
 
       if (this.multipleScenesVisible) {
