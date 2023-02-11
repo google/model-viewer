b310a42e8d84e4cbe052bd058e13e3297b917653
Emmett Lalish <elalish@google.com>
2023-02-10 16:32:20 -0800

    update DRACO version

--- packages/model-viewer/src/features/loading.ts
+++ packages/model-viewer/src/features/loading.ts
@@ -24,11 +24,10 @@ import {Constructor, throttle} from '../utilities.js';
 export type RevealAttributeValue = 'auto'|'manual';
 export type LoadingAttributeValue = 'auto'|'lazy'|'eager';
 
-export const POSTER_TRANSITION_TIME = 300;
 export const PROGRESS_BAR_UPDATE_THRESHOLD = 100;
 
 const DEFAULT_DRACO_DECODER_LOCATION =
-    'https://www.gstatic.com/draco/versioned/decoders/1.4.1/';
+    'https://www.gstatic.com/draco/versioned/decoders/1.5.6/';
 
 const DEFAULT_KTX2_TRANSCODER_LOCATION =
     'https://www.gstatic.com/basis-universal/versioned/2021-04-15-ba1c3e4/';
