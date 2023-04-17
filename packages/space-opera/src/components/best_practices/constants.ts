fe0517b209a729304045235cea682e3600870bfd
Emmett Lalish <elalish@google.com>
2023-04-17 14:35:16 -0700

    removed focus-visible polyfill

--- packages/space-opera/src/components/best_practices/constants.ts
+++ packages/space-opera/src/components/best_practices/constants.ts
@@ -23,8 +23,6 @@ export const modelViewerTemplate = `<!doctype html>
     <meta name="description" content="&lt;model-viewer&gt; template">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link type="text/css" href="./styles.css" rel="stylesheet"/>
-    <!-- OPTIONAL: The :focus-visible polyfill removes the focus ring for some input types -->
-    <script src="https://cdn.jsdelivr.net/npm/focus-visible@5.0.2/dist/focus-visible.js" defer></script>
   </head>
   <body>
     <!-- <model-viewer> HTML element -->
