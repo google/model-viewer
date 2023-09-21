f8f12ed6b002d03fca9531e3ba240be82b8abe4f
Emmett Lalish <elalish@google.com>
2023-09-20 17:01:42 -0700

    fixed variant normal map bug

--- packages/model-viewer/src/features/scene-graph/nodes/primitive-node.ts
+++ packages/model-viewer/src/features/scene-graph/nodes/primitive-node.ts
@@ -13,7 +13,7 @@
  * limitations under the License.
  */
 import {Material as ThreeMaterial, Mesh} from 'three';
-import {GLTFReference} from 'three/examples/jsm/loaders/GLTFLoader.js';
+import {GLTFParser, GLTFReference} from 'three/examples/jsm/loaders/GLTFLoader.js';
 
 import {CorrelatedSceneGraph} from '../../../three-components/gltf-instance/correlated-scene-graph.js';
 import {KHRMaterialsVariants, Primitive} from '../../../three-components/gltf-instance/gltf-2.0.js';
@@ -41,6 +41,7 @@ export class PrimitiveNode extends Node {
   public initialMaterialIdx = 0;
   private activeMaterialIdx = 0;
   private modelVariants: Map<string, VariantData>;
+  private parser: GLTFParser;
 
   constructor(
       mesh: Mesh, mvMaterials: Material[],
@@ -49,6 +50,7 @@ export class PrimitiveNode extends Node {
     super(mesh.name);
     this.mesh = mesh;
     const {gltf, threeGLTF, threeObjectMap} = correlatedSceneGraph;
+    this.parser = threeGLTF.parser;
     this.modelVariants = modelVariants;
     this.mesh.userData.variantData = modelVariants;
     // Captures the primitive's initial material.
@@ -127,8 +129,9 @@ export class PrimitiveNode extends Node {
 
   async setActiveMaterial(material: number): Promise<ThreeMaterial|null> {
     const mvMaterial = this.materials.get(material);
-    if (mvMaterial != null) {
+    if (mvMaterial != null && material !== this.activeMaterialIdx) {
       this.mesh.material = await mvMaterial[$getLoadedMaterial]();
+      this.parser.assignFinalMaterial(this.mesh);
       this.activeMaterialIdx = material;
     }
     return this.mesh.material as ThreeMaterial;
