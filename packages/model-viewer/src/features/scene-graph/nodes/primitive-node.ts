e41a79e3bb644b2c7913ef8d9ad0b66ce8f33ab2
Emmett Lalish <elalish@google.com>
2023-09-22 11:27:38 -0700

    fixed with hack

--- packages/model-viewer/src/features/scene-graph/nodes/primitive-node.ts
+++ packages/model-viewer/src/features/scene-graph/nodes/primitive-node.ts
@@ -12,7 +12,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import {Material as ThreeMaterial, Mesh} from 'three';
+import {Material as ThreeMaterial, Mesh, MeshPhysicalMaterial} from 'three';
 import {GLTFParser, GLTFReference} from 'three/examples/jsm/loaders/GLTFLoader.js';
 
 import {CorrelatedSceneGraph} from '../../../three-components/gltf-instance/correlated-scene-graph.js';
@@ -131,7 +131,15 @@ export class PrimitiveNode extends Node {
     const mvMaterial = this.materials.get(material);
     if (mvMaterial != null && material !== this.activeMaterialIdx) {
       this.mesh.material = await mvMaterial[$getLoadedMaterial]();
-      this.parser.assignFinalMaterial(this.mesh);
+      const {normalScale} = this.mesh.material as MeshPhysicalMaterial;
+      // TODO: remove this hack in favor of properly storing the different
+      // three.js materials that are potentially created for different meshes
+      // that share a glTF material.
+      if (normalScale != null &&
+          (normalScale.y * normalScale.x < 0) !=
+              (this.mesh.geometry.attributes.tangent == null)) {
+        this.parser.assignFinalMaterial(this.mesh);
+      }
       this.activeMaterialIdx = material;
     }
     return this.mesh.material as ThreeMaterial;
