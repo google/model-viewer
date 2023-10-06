64abf223a013e35cbca9c0194f520d1d61e6e524
Diego Teran <diegoteran94@gmail.com>
2023-10-06 13:41:54 -0700

    clean

--- packages/model-viewer/src/three-components/gltf-instance/gltf-2.0.ts
+++ packages/model-viewer/src/three-components/gltf-instance/gltf-2.0.ts
@@ -110,7 +110,6 @@ export type MirroredRepeatWrap = Wrap.MirroredRepeat;
 export type RepeatWrap = Wrap.Repeat;
 
 export type WrapMode = RepeatWrap|ClampToEdgeWrap|MirroredRepeatWrap;
-export type WrapMode = RepeatWrap|ClampToEdgeWrap|MirroredRepeatWrap;
 
 export interface Sampler {
   name?: string;
