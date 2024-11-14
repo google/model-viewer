0982af8a44214d99acaa818f9abe6482976e53af
samaneh-kazemi <kazemi.samaneh@gmail.com>
2024-11-13 16:58:30 -0800

    Add ModelData class

---
+++ packages/model-viewer/src/three-components/ModelData.ts
@@ -0,0 +1,17 @@
+import {Object3D} from 'three';
+
+/**
+ * A THREE.Scene object that takes a Model and CanvasHTMLElement and
+ * constructs a framed scene based off of the canvas dimensions.
+ * Provides lights and cameras to be used in a renderer.
+ */
+export class ModelData extends Object3D {
+// ModelScene is going to have child of this types
+public url: string|null = null;
+
+constructor(url: string) {
+    super();
+    this.url = url;
+    console.log("ModelData constructor is called");
+}
+}
