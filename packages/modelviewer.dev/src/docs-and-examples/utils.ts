73cd0962e56923c86d07c11af62890b188acfb11
Diego Teran <dteran@google.com>
2026-02-10 23:41:09 -0800

    fix issue #5009 (#5121)

--- packages/modelviewer.dev/src/docs-and-examples/utils.ts
+++ packages/modelviewer.dev/src/docs-and-examples/utils.ts
@@ -126,7 +126,7 @@ export function init(docsOrExample: string) {
 
 function handleSideBarClickToggle(event: any) {
   if (!(event.target.classList.contains('sidebar') || event.target.closest(".sidebar")) && !event.target.classList.contains("hamburgerInput")) {
-    if (event.target.classList.contains('tab')) {
+    if (event.target.closest('.tab')) {
       document.getElementById('sidenav')?.classList.toggle('active');
     } else {
       if (document.getElementById('sidenav')?.classList.contains('active')) {
